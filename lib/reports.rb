5dbda0d2b89c8fc4f9c652b9fd57df339d98b0c2
Michael-Meade <mameade@utica.edu>
2021-01-22 00:14:53 -0500

    SSRF Tool

---
+++ lib/reports.rb
@@ -0,0 +1,23 @@
+require 'fileutils'
+class Reports
+    def initialize(url, data)
+        @url  = url
+        @data = data
+    end
+    def data
+        @data
+    end
+    def url
+        @url
+    end
+    def save_site
+        # Creates a "reports" directory if it does not exists
+        # It will also create a directory with the name of url
+        directory = File.join("reports", url)
+        FileUtils.mkdir_p(directory)
+        # Creates a new file with the name of URL.txt
+        f = File.new(File.join(directory, "#{url}.txt"), 'a+')
+        f.write(data.concat("\n"))
+        f.close
+    end
+end
---
+++ lib/ssrf_scan.rb
@@ -0,0 +1,50 @@
+require 'typhoeus'
+require 'uri'
+require 'optparse'
+require 'colorize'
+require_relative 'reports'
+options = {}
+OptionParser.new do |o|
+  o.banner = "Usage example: test.rb --url http://localhost:4567/?url=SSRF.\n You need to put SSRF."
+  options[:banner] = o.banner
+
+  o.on("--url URL") do |d|
+    options[:url] = d
+  end
+
+
+  # sets the uo as options[:ps]
+  # need to give the program the IP
+  o.on("--ps ip", "Port scan") do |dd|
+    options[:ps] = dd
+  end
+end.parse!
+
+
+
+
+if options[:ps]
+    # all the possible ports
+    max_port = 65535
+    # combine the URL and the IP for the port
+    url      = options[:url].gsub("SSRF", options[:ps] + ":")
+    for i in 0..max_port.to_i
+        r = Typhoeus::Request.new(url + i.to_s)
+        r.on_complete do |response|
+            # get the response code
+            code = response.code.to_i
+            puts url + i .to_s
+            # if the response code has the 
+            # the status of 200
+            if code.to_i  == 200
+                data = url + i .to_s
+                uri = URI(url).host
+                Reports.new(uri, data).save_site
+            end
+        end
+        r.run
+        i+=1
+    end
+end
+
+
